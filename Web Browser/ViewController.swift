d2cf05586930635c0568b154dd623b32145a241f
asim samuel <asim.samuel1@gmail.com>
2019-07-11 22:11:58 -0400

    Initial Commit

---
+++ Web Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web Browser
+//
+//  Created by Asim on 7/11/19.
+//  Copyright © 2019 Asim Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
